3082793eb22baf9392b467b7e8d709ce00814534
Rafael Aguilar Muñoz <55810413+Rafael2026@users.noreply.github.com>
2020-12-04 19:27:01 +0100

    Add files via upload

---
+++ Museos de Málaga/scripts.js
@@ -0,0 +1,9 @@
+/** scripts de Realidad Aumentada **/
+
+function openNav() {
+    document.getElementById("myTopnav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("myTopnav").style.width = "0";
+  }
