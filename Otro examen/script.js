43dd72eb7c85ded8f312450f4c4f366f6b94db52
Rafael Aguilar Muñoz <55810413+Rafael2026@users.noreply.github.com>
2020-12-04 19:28:14 +0100

    Add files via upload

---
+++ Otro examen/script.js
@@ -0,0 +1,8 @@
+/*** scripts de Realidad Aumentada ***/
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+  
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
