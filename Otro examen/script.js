1aadf598996ff521f163ffd16c357e7d09dc53dc
Rafael Aguilar Muñoz <55810413+Rafael2026@users.noreply.github.com>
2022-03-14 10:50:05 +0100

    optimizar carpeta otro examen

--- Otro examen/script.js
+++ Otro examen/script.js
@@ -1,8 +1,7 @@
-/*** scripts de Realidad Aumentada ***/
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.width = "250px";
 }
-  
+
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.width = "0";
 }
