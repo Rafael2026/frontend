954d2d7a944e46d1da839fc8f845cccd5a04b862
Rafael Aguilar Muñoz <55810413+Rafael2026@users.noreply.github.com>
2022-03-14 10:59:13 +0100

    optimizar carpeta Alagaesia

--- Alagaesia/script.js
+++ Alagaesia/script.js
@@ -1,12 +1,10 @@
-function myFunction () {
+function myFunction() {
 
-    var x = document.getElementById ("myTopnav");
+  var x = document.getElementById("myTopnav");
 
-    if (x.className === "topnav") {
-      
-        x.className += " responsive";
-
-    } else {
-      x.className = "topnav";
-    }
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
