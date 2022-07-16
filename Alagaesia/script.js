a0d2970958a45cf621958c7f6aeca957f8fbf217
Rafael Aguilar Muñoz <rafaaguilarmunoz@gmail.com>
2022-07-16 15:36:47 +0200

    Update script.js

--- Alagaesia/script.js
+++ Alagaesia/script.js
@@ -2,9 +2,9 @@ $(".icon")[0].click = function() {
   
   var barraNav = $(".topnav")[0];
   
-  if (barraNav.attr("class") == "topnav") {
-    barraNav.addClass( "responsive");
+  if (barraNav.attr("class") === "topnav") {
+    barraNav.addClass("responsive");
   } else {
-    barraNav.attr("class", "topnav");
+    barraNav.removeClass("topnav responsive").add("topnav");
   }
 };
