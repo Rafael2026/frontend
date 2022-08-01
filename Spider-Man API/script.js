5a19218e02589381d9bfc764e7a397579400e1b8
Rafael Aguilar Muñoz <rafaaguilarmunoz@gmail.com>
2022-08-01 12:49:47 +0200

    Add files via upload

---
+++ Spider-Man API/script.js
@@ -0,0 +1,34 @@
+fetch('https://online-movie-database.p.rapidapi.com/auto-complete?q=spiderman', {
+  
+  "method": "GET",
+
+  "headers": {
+    'X-RapidAPI-Key': '57ebe41e8dmsh52a2626e5be96c5p16a615jsn387c2ff082e5',
+    'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
+  }
+
+}).then(response => response.json()).then(data => {
+  
+  //console.log(data)
+  const arrayMovies = data.d;
+
+  arrayMovies.map((element) => {
+    
+    //console.log(element)
+    const title = element.l;
+    const image = element.i.imageUrl;
+    const cast = element.s;
+      
+      const poster = `<div>
+        <img src="${image}" />
+        <h2>${title}</h2>
+        <small>${cast}</small>
+      </div>`
+
+      document.getElementById('container').innerHTML += poster;
+
+  })
+
+}).catch(err => {
+console.error(err);
+});
