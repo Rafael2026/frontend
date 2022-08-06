de162f9117707dc530d67d9f6af514f2e19a2b21
Rafael Aguilar Muñoz <rafaaguilarmunoz@gmail.com>
2022-08-06 17:32:01 +0200

    Add files via upload

---
+++ WebCam/script.js
@@ -0,0 +1,8 @@
+/* Script de WebCam */
+function openNav() {
+  document.getElementById ("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById ("mySidenav").style.width = "0";
+}
