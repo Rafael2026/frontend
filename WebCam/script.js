da2e40491f1f07b627ad6ce7aefd82946736baa3
Rafael Aguilar Muñoz <rafaaguilarmunoz@gmail.com>
2022-08-06 18:03:14 +0200

    Change four spaces to two

--- WebCam/script.js
+++ WebCam/script.js
@@ -1,8 +1,8 @@
 /* Script de WebCam */
 function openNav() {
-  document.getElementById ("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.width = "250px";
 }
 
 function closeNav() {
-  document.getElementById ("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.width = "0";
 }
