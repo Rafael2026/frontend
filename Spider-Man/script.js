5dbd0d05f535baa105f82d61fbbecb4574fd9344
Rafael Aguilar Muñoz <rafaaguilarmunoz@gmail.com>
2022-08-14 17:45:32 +0200

    Add files via upload

--- Spider-Man/script.js
+++ Spider-Man/script.js
@@ -1,20 +1,23 @@
-fetch('https://online-movie-database.p.rapidapi.com/auto-complete?q=spiderman', {
-  
-  "method": "GET",
+"use strict";
 
-  "headers": {
+/*fetch("https://online-movie-database.p.rapidapi.com/auto-complete?q=spider-man", {
+  
+  method: "GET",
+
+  headers: {
     'X-RapidAPI-Key': '57ebe41e8dmsh52a2626e5be96c5p16a615jsn387c2ff082e5',
     'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
   }
 
 }).then(response => response.json()).then(data => {
   
-  //console.log(data)
+  console.log(data);
   const arrayMovies = data.d;
 
   arrayMovies.map((element) => {
     
     //console.log(element)
+
     const title = element.l;
     const image = element.i.imageUrl;
     const cast = element.s;
@@ -31,4 +34,37 @@ fetch('https://online-movie-database.p.rapidapi.com/auto-complete?q=spiderman', 
 
 }).catch(err => {
   console.error(err);
+});*/
+
+fetch("./array.json").then(response => {
+  return response.json();
+}).then(data => {
+  console.log(data)
 });
+
+/*.then(data => {
+  
+  console.log(data);
+  const arrayMovies = data.d;
+
+  arrayMovies.map((element) => {
+    
+    //console.log(element)
+    
+    const title = element.l;
+    const image = element.i.imageUrl;
+    const cast = element.s;
+      
+      const poster = `<div>
+        <img src="${image}" />
+        <h2>${title}</h2>
+        <small>${cast}</small>
+      </div>`
+
+      document.getElementsByClassName("container")[0].innerHTML += poster;
+
+  })
+
+}).catch(err => {
+  console.error(err);
+});*/
