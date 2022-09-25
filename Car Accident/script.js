ccc904c0832a9f63dcf595b91bd6146c870eb938
Rafael2026 <rafaaguilarmunoz@gmail.com>
2022-09-25 12:27:52 +0200

    Change id by class

--- Car Accident/script.js
+++ Car Accident/script.js
@@ -1,15 +1,15 @@
 window.onload = function() {
-  coche = document.getElementById("coche");
+  coche = document.getElementsByClassName("coche");
   setInterval(conducir, 50);
   setInterval(repetir, 2250);
 }
 
 function conducir() {
   desplazar += 10;
-  coche.style.left = desplazar + "px";
+  coche[0].style.left = desplazar + "px";
 }
 
 function repetir() {
-  coche.style.left = "0px";
+  coche[0].style.left = "0px";
   desplazar = 0;
 }
