6bc2c3b3974d514d5fe2d026cd99f3daff33ae25
Rafael2026 <rafaaguilarmunoz@gmail.com>
2022-12-04 11:58:50 +0100

    Combine projects

---
+++ Index/js/accident.js
@@ -0,0 +1,17 @@
+var desplazar = 0;
+
+window.onload = function() {
+  coche = document.getElementsByClassName("coche");
+  setInterval(conducir, 50);
+  setInterval(repetir, 2250);
+};
+
+function conducir() {
+  desplazar += 10;
+  coche[0].style.left = desplazar + "px";
+}
+
+function repetir() {
+  coche[0].style.left = "0px";
+  desplazar = 0;
+}
